9619aa846e6f3b59506fe5e22b71618fabf47a0d
Pedrozo0901 <felipepedrozoribeiro@gmail.com>
2025-04-28 16:59:49 -0400

    feat: começando a fezer os controllers

---
+++ SistemaEscolarApi/Controllers/AlunoController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaEscolarApi.Models;
+using SistemaEscolarApi.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaEscolarApi.Controllers
+{
+    public class AlunoController
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class AlunoController : ControllerBase
+        {
+            
+        }
+    }
+}
