709d7b42a4ba772e071e10c5d64d009b03f447c4
Pedrozo0901 <felipepedrozoribeiro@gmail.com>
2025-04-28 16:56:32 -0400

    feat: feito os DTOs

---
+++ SistemaEscolarApi/DTO/AlunoDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaEscolarApi.DTO
+{
+    public class AlunoDTO
+    {
+        public string Nome {get; set;}
+        public string Curso {get; set;}
+        
+    }
+}
---
+++ SistemaEscolarApi/DTO/CursoDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaEscolarApi.DTO
+{
+    public class CursoDTO
+    {
+        public string Descricao {get; set;}
+    }
+}
