061dd7c55010476e1a8fecce65be1401cdffa1fe
Pedrozo0901 <felipepedrozoribeiro@gmail.com>
2025-05-05 14:20:33 -0400

    fix: Program

--- SistemaEscolarApi/Program.cs
+++ SistemaEscolarApi/Program.cs
@@ -5,6 +5,9 @@ using SistemaEscolarApi.DB;
 using FluentValidation.AspNetCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using SistemaEscolarApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,19 @@ builder.Services.AddControllers();
 builder.Services.AddAuthorization();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters 
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"])),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
 var app = builder.Build();
 
 app.UseSwagger();
@@ -23,5 +39,19 @@ app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+//para subir os arquivos estáticos
+app.UseStaticFiles();
+app.UseRouting();
+app.UseHttpsRedirection();
+
+app.MapGet("/", context => 
+{
+    context.Response.Redirect("/index.html");
+    return Task.CompletedTask; // Retorna uma tarefa concluída imediatamente
+});
+
+
+
 app.MapControllers();
 app.Run();
